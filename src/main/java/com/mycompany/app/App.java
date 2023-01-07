3d6207cfdcd582720d268f12b0fe032b687a76f0
blessedralph <ask4ralph@engineer.com>
2023-01-07 07:42:19 +0000

    another commit from cli

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World,this is FROM AJBaba ON JENKINS!!!" );( "I'm taking it a step further" );
+        System.out.println( "Hello World,this is FROM AJBaba ON JENKINS!!!" )( "I'm taking it a step further" );
     }
 }
